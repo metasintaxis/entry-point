9b75574fc8f7dd3c814707fd1847ece337323cf3
AlbertoMata <angelhz@comunidad.unam.mx>
2021-10-16 19:05:45 -0500

    Proof of concept for smooth scrooling

--- src/lwc/wired/sitio/sitio.js
+++ src/lwc/wired/sitio/sitio.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class Sitio extends LightningElement {}
+export default class Sitio extends LightningElement {
+	scrollToOne = (event) => {
+		const container = this.template.querySelector('.section3');
+		container.scrollIntoView(true);
+	};
+}
