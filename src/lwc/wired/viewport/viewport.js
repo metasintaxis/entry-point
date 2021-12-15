45941292e20aac1d2113889811e336ca616202d7
alberto <alberto@pop-os.localdomain>
2021-12-14 22:11:51 -0600

    Refactoring margins

--- src/lwc/wired/viewport/viewport.js
+++ src/lwc/wired/viewport/viewport.js
@@ -20,11 +20,11 @@ export default class Viewport extends LightningElement {
 		const sections = [...this.sections];
 
 		const siteSection = sections.find(
-			(section) => section.sectionName === 'site'
+			(section) => section.sectionName === 'SITE'
 		);
 
 		const homeSection = sections.find(
-			(section) => section.sectionName === 'home'
+			(section) => section.sectionName === 'HOME'
 		);
 
 		this.setState('sections', sections);
