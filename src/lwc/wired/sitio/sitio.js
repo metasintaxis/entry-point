b5ba1867be0cba37cd0d9ea7ee342827015400b5
AlbertoMata <angelhz@comunidad.unam.mx>
2022-04-04 06:43:47 -0500

    Site information completed

--- src/lwc/wired/sitio/sitio.js
+++ src/lwc/wired/sitio/sitio.js
@@ -8,24 +8,51 @@ const slides = [
 		headerAlignment: 'center',
 		backgroundColor: 'flatGreen',
 		content:
-			'This portal serves as a proof of concept of a website built entirely with Lightning Web Components. The site was created to showcase my skills as a frontend developer but also to experiment with new web technologies. The challenge was to create a user experience from scratch that was efficient but still intuitive.'
+			'This portal serves as a proof of concept of a website \
+			entirely built with Lightning Web Components. Although \
+			the framework is a relatively new technology, it\'s \
+			still a very powerful tool for creating web \
+			interfaces. The site itself is intended to be used as \
+			a starting point for creating more complex designs and \
+			applications. And it shows how far you can get with \
+			your customizations on the customer cloud.',
 	},
-
+	{
+		Id: 'webComponents',
+		title: 'standard web components',
+		backgroundColor: 'flatRed',
+		headerAlignment: 'end',
+		content:
+			'LWC was leveraged on top of the standard web components \
+			to create a highly customizable and extensible \
+			framework. This allows us to reuse the same components \
+			across multiple platforms in a relatively simple way. \
+			But most importantly, we can apply powerful coding \
+			techniques from other ecosystems like React or Vue to our projects, \
+			and that\'s is possible because all these technologies \
+			rely on the same programming language, which is \
+			JavaScript; but also because the markup language is \
+			close to the standard HTML for all cases. And all this have \
+			contributed to make the site possible.',
+	},
 	{
 		Id: 'css',
 		title: 'css is awesome',
 		headerAlignment: 'start',
 		backgroundColor: 'flatYellow',
 		content:
-			'This portal serves as proof of concept of a website built entirely with Lightning Web Components. This technology is a framework developed by Salesforce on top of the Web Components standard, allowing to reuse interfaces inside and outside of the platform. The main target of this technology is the customer experience cloud. But as you can see here, it also can be used to create standalone sites and applications.'
-	},
-	{
-		Id: 'wired',
-		title: 'the self in the wired',
-		backgroundColor: 'flatRed',
-		headerAlignment: 'end',
-		content:
-			'The Wired is a metaphor in the Serial Experiments Lain series that represents a dream/desire of individuals to materialize themselves in an omnipresent digital world. Since The Wired eventually gets merged with the physical world, the individuals that can manipulate the Wired could also modify the reality that initially scared them. I think that any profile in our network responds to this desire of being present in every place, at any time, even in the long past of future generations.'
+			'The look and feel of the site is supported by some of \
+			the most interesting additions to the CSS language \
+			like CSS Animations and CSS Variables. It would be \
+			simpler to adopt a framework to handle all this job, \
+			but standard CSS provides plenty of a room for experimentation. \
+			CSS has been seen as a hard language to learn, but \
+			once you get deep into the technology, you can appreciate \
+			how robust it is. However, the slds frameworks is \
+			still used in this site for layouts, mainly \
+			because that\'s how you would tipically work in \
+			your daily projects. '
+			
 	}
 ];
 
